1853677167cbe6016dcf2d119156ae3f3ef86c20
Dejan Pelzel <info@dejan-pelzel.com>
2016-01-23 02:17:49 +0100

    updated the example

--- BunnyWay.Metrics.Example/Program.cs
+++ BunnyWay.Metrics.Example/Program.cs
@@ -12,21 +12,23 @@ namespace BunnyWay.Metrics.Example
     {
         static void Main(string[] args)
         {
+            // Initializes the MetricsTracker.Default object
             MetricsTracker.InitializeDefault();
 
-            // Track a counter metric
+            // Track a counter metric, it will simply be incremented by 1
             MetricsTracker.Default.TrackCounter("WebApp.Login.FailedLoginAttempt");
 
-            // Track a counter metric
+            // Track a counter metric, increment it by a set amount
             MetricsTracker.Default.TrackCounter("Server.RequestsServed", 20);
 
-            // Track a metric
+            // Track a metric, each of these is uniquely registered
             MetricsTracker.Default.TrackMetric("WebApp.ImageProcessing.QueueLength", 230);
 
-            // Track with tags
+            // Track a metric with tags
             MetricsTracker.Default.TrackMetric("WebApp.EmailQueue.QueueLength", 230, new Tag("ServerName", "as1"), new Tag("ServerGroup", "2"));
 
-            // Simple way to track timing
+            // Simple way to track timing, just make sure that the object returned by 
+            // MetricsTracker.Default.TrackTimeMetric is disposed when the tracker should be stopped
             using (var timer = MetricsTracker.Default.TrackTimeMetric("WebApp.PasswordGeneration.Time"))
             {
                 Thread.Sleep(1000);
